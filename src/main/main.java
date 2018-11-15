b49ad4c26a87fd88ba0220e30fdc003637485020
Usuario <Usuario@10.22.148.82>
2018-11-15 11:34:25 +0000

    Kata 4 versión 1

---
+++ src/main/main.java
@@ -0,0 +1,23 @@
+package main;
+
+import java.io.IOException;
+import java.util.List;
+import model.Histogram;
+import model.Mail;
+import view.HistogramDisplay;
+import view.MailHistogramBuilder;
+import view.MailListReader;
+
+public class main {
+
+    public static void main(String args[]) throws IOException{
+        
+        
+        String filename = "C:\\Users\\Usuario\\Documents\\NetBeansProjects\\Kata4\\email.txt";
+        List<Mail> mailList = MailListReader.read(filename);
+        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
+        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
+        histoDisplay.execute();
+    }
+    
+}
